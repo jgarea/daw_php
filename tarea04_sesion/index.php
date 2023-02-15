6c88e1f54f9236fbb1cf7d79a17dd39436942e98
Juan Eugenio Antón Area <jgntra@gmail.com>
2023-02-15 17:04:05 +0100

    añadir preferencias.php

--- tarea04_sesion/index.php
+++ tarea04_sesion/index.php
@@ -7,27 +7,130 @@
     <title>Tarea04</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-
 </head>
 <body>
-    
+<?php
+
+if (!empty($_REQUEST)) {
+  // Iniciamos la sesión o recuperamos la anterior sesión existente
+  session_start();
+  // Comprobamos si la variable ya existe
+  $_SESSION['idioma']=$_REQUEST['idioma'];
+  $_SESSION['publico']=$_REQUEST['publico'];
+  $_SESSION['zona_h']=$_REQUEST['zona_h'];
+  
+}
+?>  
 <div class="container col-4 mt-3">
-    <div class="mb-3">
-      <label for="formGroupExampleInput" class="form-label">Example label</label>
-      <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
-    </div>
-    <div class="mb-3">
-      <label for="formGroupExampleInput2" class="form-label">Another label</label>
-      <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
-    </div>
-    <div class="mb-3">
-      <label for="formGroupExampleInput2" class="form-label">Another label</label>
-      <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
-    </div>
-    <div>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Button</a>
-        <a name="" id="" class="btn btn-secondary" href="#" role="button">Button</a>
-    </div>
+    <?php
+      if (!empty($_POST))
+      echo "<p>Preferencia de usuario guardadas</p>";
+    ?>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+      <div class="mb-3">
+        <label for="idioma" class="form-label">Idioma</label>
+        <select class="form-select form-select-lg" name="idioma" id="idioma">
+        <?php
+          if (isset($_SESSION['idioma'])) { 
+            if($_SESSION['idioma']=="ingles") { ?>
+            <option value="español">Español</option>
+            <option value="ingles" selected>Inglés</option>
+          <?php } else if ($_SESSION['idioma']=="español") { ?>
+            <option value="español" selected>Español</option>
+            <option value="ingles">Inglés</option>
+          <?php } } else {  ?>
+            <option value="español">Español</option>
+            <option value="ingles">Inglés</option>
+          <?php } ?>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label for="publico" class="form-label">Perfil público</label>
+        <select class="form-select form-select-lg" name="publico" id="publico">
+          <?php
+          if (isset($_SESSION['publico'])) { 
+            if($_SESSION['publico']=="no") { ?>
+            <option value="si">Si</option>
+            <option value="no" selected>No</option>
+          <?php } else if ($_SESSION['publico']=="si") { ?>
+            <option value="si" selected>Si</option>
+            <option value="no">No</option>
+          <?php } } else {  ?>
+            <option value="si">Si</option>
+            <option value="no">No</option>
+          <?php } ?>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label for="zona_h" class="form-label">Zona horaria</label>
+        <select class="form-select form-select-lg" name="zona_h" id="zona_h">
+          <?php 
+          switch ($_SESSION['zona_h']) { 
+            
+            case 'GMT-2':
+            ?>
+              <option value="GMT-2" selected>GMT-2</option>
+              <option value="GMT-1">GMT-1</option>
+              <option value="GMT">GMT</option>
+              <option value="GMT+1">GMT+1</option>
+              <option value="GMT+2">GMT+2</option>
+              <?php
+              break; ?><?php
+            case 'GMT-1':
+            ?>
+              <option value="GMT-2">GMT-2</option>
+              <option value="GMT-1" selected>GMT-1</option>
+              <option value="GMT">GMT</option>
+              <option value="GMT+1">GMT+1</option>
+              <option value="GMT+2">GMT+2</option>
+              <?php
+              break; ?><?php
+            case 'GMT':
+            ?>
+              <option value="GMT-2">GMT-2</option>
+              <option value="GMT-1">GMT-1</option>
+              <option value="GMT" selected>GMT</option>
+              <option value="GMT+1">GMT+1</option>
+              <option value="GMT+2">GMT+2</option>
+              <?php
+              break; ?><?php
+            case 'GMT+1':
+            ?>
+              <option value="GMT-2">GMT-2</option>
+              <option value="GMT-1">GMT-1</option>
+              <option value="GMT">GMT</option>
+              <option value="GMT+1" selected>GMT+1</option>
+              <option value="GMT+2">GMT+2</option>
+              <?php
+              break; ?><?php
+            case 'GMT+2':
+            ?>
+              <option value="GMT-2">GMT-2</option>
+              <option value="GMT-1">GMT-1</option>
+              <option value="GMT">GMT</option>
+              <option value="GMT+1">GMT+1</option>
+              <option value="GMT+2" selected>GMT+2</option>
+              <?php
+              break; ?>
+            
+              <?php
+            default:?>
+              <option value="GMT-2">GMT-2</option>
+              <option value="GMT-1">GMT-1</option>
+              <option value="GMT">GMT</option>
+              <option value="GMT+1">GMT+1</option>
+              <option value="GMT+2">GMT+2</option>
+              <?php break;
+          }
+          ?>
+          
+        </select>
+      </div>
+      <div>
+          <button class="btn btn-primary" type="submit">Establecer preferencias</button>
+          <a name="" id="" class="btn btn-success" href="mostrar.php" role="button">Mostrar preferencias</a>
+      </div>
+    </form>
 </div>
 <?php
 
