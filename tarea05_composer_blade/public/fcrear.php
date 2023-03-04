c267e71997e10080f48a54bde147210b33ea386e
Juan Eugenio Antón Area <jgntra@gmail.com>
2023-03-04 12:14:37 +0100

    Add fcrear.php

---
+++ tarea05_composer_blade/public/fcrear.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+require '../vendor/autoload.php';
+
+use Philo\Blade\Blade;
+
+$views = '../views';
+$cache = '../cache';
+$blade = new Blade($views, $cache);
+
+$titulo     = 'Nuevo';
+$encabezado = 'Crear Jugador';
+
+if (isset($_SESSION['error'])) {
+    $error = $_SESSION['error'];
+    echo $blade
+        ->view()
+        ->make('vcrear', compact('titulo', 'encabezado', 'error'))
+        ->render();
+    unset($_SESSION['error']);
+} else if (isset($_SESSION['codigo'])) {
+    $code = $_SESSION['codigo'];
+    unset($_SESSION['codigo']);
+    echo $blade
+        ->view()
+        ->make('vcrear', compact('titulo', 'encabezado', 'code'))
+        ->render();
+} else {
+    echo $blade
+        ->view()
+        ->make('vcrear', compact('titulo', 'encabezado'))
+        ->render();
+}
