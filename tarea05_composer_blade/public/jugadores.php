3133f090c1a4b8642adf55534d857903afa4f750
Juan Eugenio Antón Area <jgntra@gmail.com>
2023-03-04 11:45:53 +0100

    Add jugadores.php

---
+++ tarea05_composer_blade/public/jugadores.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+require '../vendor/autoload.php';
+
+use Clases\Jugadores;
+use Millon\Barcode\DNS1D;
+use Philo\Blade\Blade;
+
+$views = '../views';
+$cache = '../cache';
+$blade = new Blade($views, $cache);
+
+$d          = new DNS1D();
+$titulo     = 'Jugadores';
+$encabezado = 'Listado de Jugadores';
+$jugadores  = (new Jugadores())->recuperarJugadores();
+
+$d->setStorPath($cache);
+
+if(isset($_SESSION['mensaje'])){
+    $mensaje = $_SESSION['mensaje'];
+    unset($_SESSION['mensaje']); //Para no volver repetir el mensaje
+    echo $blade
+    ->view()
+    ->make('vjugadores', compact('titulo', 'encabezado','jugadores','d','mensaje'))
+    ->render();
+} else {
+    echo $blade
+    ->view()
+    ->make('vjugadores', compact('titulo', 'encabezado','jugadores','d'))
+    ->render();
+}
+
