2bacae22b8cf32d7d3a4a1eb4fc3ca56a0d6e288
Juan Eugenio Antón Area <jgntra@gmail.com>
2023-04-22 15:08:39 +0200

    Update tarea06

--- tarea06_SOAP/public/cliente.php
+++ tarea06_SOAP/public/cliente.php
@@ -1,5 +1,8 @@
 <?php
-$url = 'http://localhost/unidad06_SOAP/servidorSoap/servicio.php';
+require '../vendor/autoload.php';
+
+//require '../servidorSoap/servidor.php';
+$url = 'http://localhost/unidad06_SOAP/servidorSoap/servidor.php';
 $uri = 'http://localhost/unidad06_SOAP/servidorSoap';
 //$paramSaludo = ['texto' => "Manolo"];
 //$param = ['a' => 51, 'b' => 29];
@@ -11,6 +14,6 @@ try {
 $saludo = $cliente->__soapCall('getFamilias', array());
 //$suma = $cliente->__soapCall('suma', $param);
 //$resta=$cliente->__soapCall('resta', $param);
-echo $saludo[0]. " La suma es";
+echo $saludo[0]. "Prueba";
 //También podríamos hacer
 //$saludo=$cliente->saludo("Manolo");
